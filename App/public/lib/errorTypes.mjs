279fa50baba5244ebcf3e9d0364e926ac64ecb74
troppdus <troppdus@b-tu.de>
2023-07-04 16:33:16 +0200

    added errorTypes

--- App/public/lib/errorTypes.mjs
+++ App/public/lib/errorTypes.mjs
@@ -1,12 +1,72 @@
-/**
- * @fileOverview  Defines error classes (also called "exception" classes)
- * for property constraint violations
- * @author Gerd Wagner
- */
-class ConstraintViolation {
-  constructor(msg) {
-    this.message = msg;
+class ConstraintViolation extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "ConstraintViolation";
   }
 }
 
-export { ConstraintViolation };
+class NoConstraintViolation extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "NoConstraintViolation";
+  }
+}
+
+class MandatoryValueConstraintViolation extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "MandatoryValueConstraintViolation";
+  }
+}
+
+class RangeConstraintViolation extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "RangeConstraintViolation";
+  }
+}
+
+class UniquenessConstraintViolation extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "UniquenessConstraintViolation";
+  }
+}
+
+class StringLenghtConstrainViolation extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "StringLenghtConstrainViolation";
+  }
+}
+
+class IntervalConstraintViolation extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "IntervalConstraintViolation";
+  }
+}
+
+class ReferentialIntegrityConstraintViolation extends Error {
+  constructor (msg) {
+    super( msg);
+    this.name = "ReferentialIntegrityConstraintViolation";
+  }
+}
+
+class FrozenValueConstraintViolation extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "FrozenValueConstraintViolation";
+  }
+}
+
+export { NoConstraintViolation, 
+  MandatoryValueConstraintViolation, 
+  RangeConstraintViolation,
+  UniquenessConstraintViolation,
+  StringLenghtConstrainViolation,
+  IntervalConstraintViolation,
+  ReferentialIntegrityConstraintViolation,
+  FrozenValueConstraintViolation,
+  ConstraintViolation};
